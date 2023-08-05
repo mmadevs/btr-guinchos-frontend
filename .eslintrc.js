5b48e432f644fa1283a8cc014c8b4dcaf6d9897d
MMA Devs <mmadevsys@gmail.com>
2023-08-05 14:06:19 -0300

    config: set eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react"
+    ],
+    "rules": {
+        indent:"off",
+        "prettier/prettier": [
+            'error',
+            {
+                endOfLine: 'auto',
+				useTabs: true,
+				trailingComma: 'none',
+				semi: false,
+				singleQuote: true,
+				jsxSingleQuote: true,
+				tabWidth: 4,
+				printWidth: 80
+            }
+        ]
+    }
+}
