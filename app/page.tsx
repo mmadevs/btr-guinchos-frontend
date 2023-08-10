3ca478bbeb27e557c1f57da1c7ebf91450e0a988
MMA Devs <mmadevsys@gmail.com>
2023-08-10 15:31:20 -0300

    push to login automatically

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { extendTheme } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
 
 const theme = extendTheme({
 	breakpoints: {
@@ -12,6 +13,9 @@ const theme = extendTheme({
 	}
 })
 export default function Home() {
+	const router = useRouter()
+
+	router.push('/login')
 	return (
 		<main className='flex flex-col items-start bg-zinc-900 w-full h-full'>
 			voce n logou ainda
